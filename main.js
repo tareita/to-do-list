7d89a5433a23338aad03e72131546f4e67d6adfd
tareita <tareitaa@outlook.com>
2022-06-01 22:27:54 +0100

    implented a way of deleting items from the to do list

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 let tasks = []
 const textField = document.getElementById("textField")
 const list = document.getElementById("list")
-
+let counter = 0
 
 function addToList() {
-    const textValue = textField.value
+    const text = textField.value
     textField.value = ""
-    tasks.push(textValue)
+    const task = {text:text, id:counter}
+    counter++
+    tasks.push(task)
+    setList()
+}
+
+function deleteFromList(id) { 
+    let indexToDelete 
+    for (let i=0;i<tasks.length;i++){
+        const task = tasks[i]
+        if (task.id == id) {
+            indexToDelete = i  
+            break   
+        }      
+    }
+    tasks.splice(indexToDelete)
     setList()
 }
 
@@ -14,8 +29,11 @@ function setList() {
     let innerHTML = ""
     for (let i=0;i<tasks.length;i++) {
         const task = tasks[i]
-        innerHTML += "<li>" + task + "</li>"    
+        innerHTML += "<li id=" + task.id + ">" + task.text + "  <button onclick='deleteFromList("+task.id+")'> x </button> </li>"    
     }
     
     list.innerHTML=innerHTML
 }
+
+
+
