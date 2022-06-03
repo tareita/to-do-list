db764fb278f936b064253e282a34b9da7037893d
tareita <tareitaa@outlook.com>
2022-06-03 19:56:57 +0100

    adding update functionality

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let counter = 0
 function addToList() {
     const text = textField.value
     textField.value = ""
-    const task = {text:text, id:counter}
+    const task = {text:text, id:counter, beingEdited:false}
     counter++
     tasks.push(task)
     setList()
@@ -25,11 +25,33 @@ function deleteFromList(id) {
     setList()
 }
 
+//update list - have a button for each item in the list that says update
+
+function toggleEdit(id) {
+    let indexToUpdate
+    for (let i=0;i<tasks.length;i++){
+        const task = tasks[i]
+        if (task.id == id) {
+            indexToUpdate = i  
+            break   
+        }      
+    }
+    beingEdited = true
+    setList()
+    const newText = editTextField.value
+    tasks[indexToUpdate] = newText
+    //beingEdited = false
+}
+
 function setList() {
     let innerHTML = ""
     for (let i=0;i<tasks.length;i++) {
         const task = tasks[i]
-        innerHTML += "<li id=" + task.id + ">" + task.text + "  <button onclick='deleteFromList("+task.id+")' class='delete-button'> x </button> </li>"    
+        innerHTML += "<li id=" + task.id + ">" + task.text + "  <button onclick='deleteFromList("+task.id+")' class='delete-button'> x </button> <button onclick='toggleEdit("+task.id+")'> edit </button> "    
+        if (task.beingEdited){
+            innerHTML += "<input type='text' id='editTextField'> <button> change </button>" 
+        }
+        innerHTML += "</li>"
     }
     
     list.innerHTML=innerHTML
